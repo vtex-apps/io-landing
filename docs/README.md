116a86caa32507d2d7c0f7e340095329ea70de8b
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-07-03 15:35:26 -0300

    Init repo with code from vtex-apps/landing-pages

---
+++ docs/README.md
@@ -0,0 +1,5 @@
+# VTEX IO
+
+It's our serverless development platform, designed to help you create scalable, production-ready web applications – and never have to worry about any infrastructure complexity again.
+
+This repository is for the source code of the VTEX IO landing on https://vtex.io.
---
+++ react/IO.tsx
@@ -0,0 +1,43 @@
+import React, { Fragment, FunctionComponent } from 'react'
+import { injectIntl, InjectedIntlProps } from 'react-intl'
+import { Helmet } from 'vtex.render-runtime'
+
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import CardGrid from './components/CardGrid'
+import ProductDisplay from './components/Product'
+import HowItWorks from './components/HowItWorks'
+import EvolutionSection from './components/Evolution'
+import KeyFeatures from './components/KeyFeatures'
+import StartBuilding from './components/StartBuilding'
+import Footer from './components/Footer'
+import favicon from './images/favicon.png'
+
+const Landing: FunctionComponent<InjectedIntlProps> = ({ intl }) => {
+  return (
+    <Fragment>
+      <Helmet>
+        <title>{intl.formatMessage({ id: 'io.page-title' })}</title>
+        <meta
+          name="description"
+          content="A serverless development platform from VTEX."
+        />
+        <meta name="theme-color" content="#F71963" />
+        <link rel="icon" href={favicon} />
+      </Helmet>
+      <Navbar />
+      <main className="w-100">
+        <Hero />
+        <CardGrid />
+        <ProductDisplay />
+        <HowItWorks />
+        <EvolutionSection />
+        <KeyFeatures />
+        <StartBuilding />
+      </main>
+      <Footer />
+    </Fragment>
+  )
+}
+
+export default injectIntl(Landing)
